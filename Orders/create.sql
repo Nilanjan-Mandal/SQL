34c534607b54ce04ce27e3b14d9123adbe24dfb8
Nilanjan Mandal <nmandalstu16@hotmail.com>
2024-06-07 12:26:11 +0530

    Modify

---
+++ Orders/create.sql
@@ -0,0 +1,13 @@
+#Create a Table
+
+create table orders
+  (
+         order_id INT NOT NULL, 
+        ,order_date DATE
+        ,order_customer_id BIGINT (10)
+        ,order_status varchar
+  );
+
+  
+
+
---
+++ Orders/load.sql
@@ -0,0 +1,7 @@
+Load table with the data. 
+
+
+LOAD DATA LOCAL INFILE /Users/nilanjan/Google Drive/My Drive/IT/Data Engineering /Bigdata/Spark/DataSets/orders.csv INTO TABLE orders;
+
+
+LOAD DATA LOCAL INFILE 'Yelp_ALL/ratings.txt' INTO TABLE ratings FIELDS TERMINATED BY '::' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' IGNORE 1 LINES (userid, itemid, ratings)"
