e3f8e1367c8dd26211d78649faaed4f2defeaea8
Nilanjan Mandal <nmandalstu16@hotmail.com>
2024-07-22 11:33:39 +0530

    Update

--- Revision/Set1.sql
+++ Revision/Set1.sql
@@ -1,2 +1,50 @@
-use Practice;
 show TABLES
+
+
+select * from employee;
+SELECT DEPT_NAME, MAX(salary) from employee GROUP BY DEPT_NAME;
+
+--rank & dense_rank
+select e.*, 
+rank() OVER (PARTITION BY DEPT_NAME ORDER BY SALARY DESC) as rnk
+from employee as e;
+
+select e.*, 
+dense_rank() OVER (PARTITION BY DEPT_NAME ORDER BY SALARY DESC) as rnk
+from employee as e;
+
+-- Max Salary using window function
+select * from (
+            select e.*, 
+            dense_rank() OVER (PARTITION BY DEPT_NAME ORDER BY SALARY DESC) as rnk
+            from employee as e
+) as x
+WHERE x.rnk = 1;
+
+-- CTE
+WITH max_sal as (
+            select e.*, 
+            dense_rank() OVER (PARTITION BY DEPT_NAME ORDER BY SALARY DESC) as rnk
+            from employee as e
+)
+SELECT * from max_sal WHERE rnk > 1;
+
+-- lead & lag 
+select e.*,
+lag(salary) over(partition by dept_name order by emp_id) as prev_empl_sal,
+lead(salary) over(partition by dept_name order by emp_id) as next_empl_sal
+from employee e;
+
+
+-- FIRST_VALUE 
+
+select *,
+first_value(product_name) over(partition by product_category order by price desc) as most_exp_product
+from product;
+
+
+select * from Employees
+
+SELECT ID, count(*) as cn
+from Employees GROUP BY ID HAVING cn > 1
+
